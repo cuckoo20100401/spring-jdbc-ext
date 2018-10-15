a08167e831b9bd334d520b74e8b01a5d952c21c7
yanglianzhi <yanglianzhi@PC201610271222>
2018-10-15 17:28:43 +0800

    测试JSqlParser开源库

--- src/main/java/org/cuckoo/springframework/jdbc/SQLParser.java
+++ src/main/java/org/cuckoo/springframework/jdbc/SQLParser.java
@@ -1,22 +1,110 @@
 package org.cuckoo.springframework.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.AllColumns;
+import net.sf.jsqlparser.statement.select.Join;
+import net.sf.jsqlparser.statement.select.OrderByElement;
+import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
+import net.sf.jsqlparser.statement.select.SelectItem;
+import net.sf.jsqlparser.statement.select.SelectItemVisitor;
+import net.sf.jsqlparser.statement.select.SelectItemVisitorAdapter;
+import net.sf.jsqlparser.statement.select.SelectVisitor;
+import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
+import net.sf.jsqlparser.statement.select.SetOperationList;
+import net.sf.jsqlparser.statement.select.WithItem;
 import net.sf.jsqlparser.util.SelectUtils;
 
 public class SQLParser {
-
-	public void test() throws JSQLParserException {
-		
-		String sql = "select * from sys_user";
+	
+	private String paginationSQL;
+	private String totalSQL;
+	
+	public void test1(String sql) throws JSQLParserException {
 		
 		Statement statement = CCJSqlParserUtil.parse(sql);
 		if (statement instanceof Select) {
 			Select selectStatement = (Select) statement;
-			System.out.println(selectStatement.toString());
+			PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+			
+			List<SelectItem> selectItems = new ArrayList<>();
+			plainSelect.setSelectItems(selectItems);
+			
+			SelectVisitor iii = new SelectVisitorAdapter();
+			SetOperationList aaa = new SetOperationList();
+			
+			selectStatement.getSelectBody().accept(iii);
+			
 		}
+	}
+
+	public void test(String sql) throws JSQLParserException {
+		
+		String srcSql = null;
+		String srcSqlSelectItems = null;
+		String srcSqlFromItem = null;
+		String srcSqlJoins = null;
+		String srcSqlWhere = null;
+		String srcSqlOrderBy = null;
+		
+		// parse
+		Statement statement = CCJSqlParserUtil.parse(sql);
+		if (statement instanceof Select) {
+			Select selectStatement = (Select) statement;
+			PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
+			//srcSql
+			srcSql = selectStatement.toString();
+			//srcSqlSelectItems
+			StringBuilder sb = new StringBuilder();
+			List<SelectItem> selectItems = plainSelect.getSelectItems();
+			for (SelectItem selectItem: selectItems) {
+				if (sb.length() == 0) {
+					sb.append(selectItem.toString());
+				} else {
+					sb.append(", ").append(selectItem.toString());
+				}
+			}
+			srcSqlSelectItems = sb.toString();
+			//srcSqlFromItem
+			srcSqlFromItem = plainSelect.getFromItem().toString();
+			//srcSqlJoins
+			List<Join> joins = plainSelect.getJoins();
+			for (Join join: joins) {
+				if (srcSqlJoins == null) {
+					srcSqlJoins = join.toString();
+				} else {
+					srcSqlJoins += " "+join.toString();
+				}
+			}
+			//srcSqlWhere
+			srcSqlWhere = plainSelect.getWhere().toString();
+			//srcSqlOrderBy
+			List<OrderByElement> orderByElements = plainSelect.getOrderByElements();
+			for (OrderByElement orderByElement: orderByElements) {
+				if (srcSqlOrderBy == null) {
+					srcSqlOrderBy = orderByElement.toString();
+				} else {
+					srcSqlOrderBy += ","+orderByElement.toString();
+				}
+			}
+			//paging
+			//plainSelect.setLimit(limit);
+		}
+		
+		// build
+		this.totalSQL = new StringBuilder()
+				.append("SELECT COUNT(*) FROM")
+				.append(srcSqlFromItem)
+				.append(srcSqlJoins)
+				.append(srcSqlWhere)
+				.append(srcSqlOrderBy)
+				.toString();
 	}
 	
 }
--- src/main/java/org/cuckoo/springframework/jdbc/ext/App.java
+++ src/main/java/org/cuckoo/springframework/jdbc/ext/App.java
@@ -2,16 +2,18 @@ package org.cuckoo.springframework.jdbc.ext;
 
 import org.cuckoo.springframework.jdbc.SQLParser;
 
-/**
- * Hello world!
- *
- */
+import net.sf.jsqlparser.JSQLParserException;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws JSQLParserException
     {
-        System.out.println( "Hello World!" );
-        
-        SQLParser test = new SQLParser();
+    	
+    	String sql1 = "select id,username,password from sys_user where age > 20";
+    	String sql2 = "select m.*,s1.id as s1id,s1.name as s1name from sys_user m left join sys_role s1 on m.sys_role_id = s1.id where m.age > 20 and s1.name = ?";
+    	String sql = "select m.*,s1.id as s1id,s1.name as s1name,(select title from article where id = m.id) as articleId from sys_user m left join sys_role s1 on m.sys_role_id = s1.id left join city s2 on m.city_id = s2.id where m.age > 20 and s1.name = ? or m.article_id in (select id from article where sort > m.age) order by m.name,m.age desc";
+    	
+        SQLParser parser = new SQLParser();
+        parser.test(sql);
     }
 }
